947f981a122c16bc789ebc041856c79ad71573a3
rosemarymagoma99@gmail.com <rosemarymagoma99@gmail.com>
2018-09-26 18:35:23 +0300

    add buttons to the html document

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//business logic
+  function Contact(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  }
+  
+  Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+
+  // user interface logic
+  $(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+      $(".contact").last().click(function() {
+    $("#show-contact").show();
+    $("#show-contact h2").text(newContact.firstName);
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+  });
+
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+    });
+  });
